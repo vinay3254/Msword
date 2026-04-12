99b3ea0eb10147e027c96b2450a4c1de75662fc4
vinay3254 <vinaygk219@gmail.com>
2026-04-12 20:34:05 +0530

    feat: wire ThemeProvider and flash-prevention script

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './theme.css';
 import './index.css';
+import { ThemeProvider } from './context/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ThemeProvider>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
